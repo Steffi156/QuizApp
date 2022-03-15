2e6fb0794442c581368be329afe1c342331a2151
Your Name <you@example.com>
2022-03-15 09:55:15 +0100

    js json

---
+++ script.js
@@ -0,0 +1,29 @@
+let questions = [
+    {
+        "image": "img/herbs.jpg",
+        "question": "Ist Breitwegerrich giftig?",
+        "answer_1": "Nein, den kann man essen wie Salat.",
+        "answer_2": "Roh ein wenig, aber gekocht kann man ihn doch geniesen.",
+        "answer_3": "Kann starke Margenkräpfe und Ausschlag verursachen, am besten nicht anfassen.",
+        "answer_4": "Aber so was von, dem sollte man nicht zu nah kommen.",
+        "right_answer": 1
+    },
+    {
+        "image": "img/fingerhut.jpg",
+        "question": "Ist Breitwegerrich giftig?",
+        "answer_1": "Nein, den kann man essen wie Salat.",
+        "answer_2": "Roh ein wenig, aber gekocht kann man ihn doch geniesen.",
+        "answer_3": "Kann starke Margenkräpfe und Ausschlag verursachen, am besten nicht anfassen.",
+        "answer_4": "Aber so was von, dem sollte man nicht zu nah kommen.",
+        "right_answer": 3
+    },    
+    {
+        "image": "img/fingerhut.jpg",
+        "question": "Frage?",
+        "answer_1": "Antwort_1",
+        "answer_2": "Antwort_2",
+        "answer_3": "Antwort_3",
+        "answer_4": "Antwort_4",
+        "right_answer": 3
+    },
+]
