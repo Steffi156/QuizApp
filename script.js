5434f8e27a3db07cda4d8d04ef01136eeca67b5c
Your Name <you@example.com>
2022-03-15 13:22:17 +0100

    at answer function

--- script.js
+++ script.js
@@ -24,6 +24,26 @@ let questions = [
         "answer_2": "Antwort_2",
         "answer_3": "Antwort_3",
         "answer_4": "Antwort_4",
-        "right_answer": 3
+        "right_answer": 2
     },
 ]
+
+let nowQuestion = 0;
+
+function init() {
+    document.getElementById('numberOfAllQuestions').innerHTML = questions.length; 
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[nowQuestion];
+    document.getElementById('answer1').innerHTML = question['answer_1'];
+    document.getElementById('answer2').innerHTML = question['answer_2'];
+    document.getElementById('answer3').innerHTML = question['answer_3'];
+    document.getElementById('answer4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let
+    document.getElementById('answer1').classList = add.
+}
